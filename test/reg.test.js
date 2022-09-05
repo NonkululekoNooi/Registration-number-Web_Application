b148303fa755e3bf93d33354863fd641f21f163a
Nonkululeko Nooi <nooi9423@gmail.com>
2022-09-05 09:51:12 +0200

    added

--- test/reg.test.js
+++ test/reg.test.js
@@ -70,18 +70,46 @@ it('should return registrations numbers that are filtered "WELLINGTON"', async f
   assert.deepEqual([{"identity_id": 3, "reg_numbers": "CN 125-898"}], output)
 
 })
+
+// it('should return registrations numbers that are filtered "SHOW ALL"', async function(){
+//   let regNo =myReg(db)
+ 
+//   let output = await regNo.filtered("SHOW ALL")
+
+//   await regNo.getRegistration("CN 125-898")
+//   await regNo.getRegistration("CJ 130-012")
+//   await regNo.getRegistration("CA 802-541")
+
+//   assert.deepEqual([{"identity_id": 3, "reg_numbers": "CN 125-898"}], output)
+
+// })
 })
 
 describe("All the registration numbers that are added", function () {
 it("should return registration numbers that are stored",async function () {
   const regNo = myReg(db);
-  await regNo.addingReg([{"reg_numbers": "CN 125-898"}]);
-
+  await regNo.addingReg([{"reg_numbers": "CN 125-898"}])
   assert.deepEqual( [{"reg_numbers": "CN 125-898"}]
   ,await regNo.getRegistration([{"reg_numbers": "CN 125-898"}]))
   
 });
 });
+
+describe("All the registration number should be reseted", async function (){
+  it("should reset all the registration numbers from the database", async function (){
+   
+    const regNo = myReg(db);
+
+    await regNo.storedRegistration('CJ 130-012');
+    await regNo.rested()
+
+    assert.equal([],await regNo.getRegistration())
+
+  })
+})
+
+
+
 
 
 
